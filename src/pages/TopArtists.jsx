6ee3821c7afe5c04dc4f829e577b4fe941b64caa
Alexander <alexadedeji15@gmail.com>
2022-10-29 12:00:53 +0100

    Top artists Page

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,26 @@
-const TopArtists = () => <div>TopArtists</div>;
+import { Error, Loader } from "../components";
+import ArtistCard from "../components/ArtistCard";
+import { useGetTopWorldChartsQuery } from "../redux/services/shazamCore";
+
+const TopArtists = () => {
+  const { data, isFetching, error } = useGetTopWorldChartsQuery();
+
+  if (isFetching) <Loader title="Loading Songs Around You" />;
+
+  if (error) <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Top Artists
+      </h2>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((track) => (
+          <ArtistCard key={track.key} track={track} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default TopArtists;
